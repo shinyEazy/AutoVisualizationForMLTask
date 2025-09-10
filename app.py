0c591914ff034ac19691f248008b18895d5e8d5d
shinyEazy <tanhngusi39@gmail.com>
2025-09-10 15:54:07 +0700

    f

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-app.py
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch(share=True)
